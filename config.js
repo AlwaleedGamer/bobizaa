e005d2c6a3b6133ff041abf791700002d832a6d0
AlwaleedGamer <149424523+AlwaleedGamer@users.noreply.github.com>
2024-06-24 00:26:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['212717457920', 'bobiza', true],
+  ['249967264949', 'bobiza', true],
 
-  ['212717457920','bobiza', true],
-  ['212717457920'],
-  ['212717457920']
+  ['249967264949','bobiza', true],
+  ['249967264949'],
+  ['249967264949']
 ];
 
-global.suittag = ['212717457920'];
-global.prems = ['212717457920'];
+global.suittag = ['249967264949'];
+global.prems = ['249967264949'];
 
 global.packname = 'bobiza bot';
 global.author = 'bobiza';
